e2a363c2da2b39d4fc11d331c8cf5cdbfd28eacb
Ezra Bradford <ezra.bradford@gmail.com>
2009-03-26 18:00:01 -0700

    newer database backup

--- b18_3214751_skybug.sql
+++ b18_3214751_skybug.sql
@@ -2,8 +2,8 @@
 -- version 3.1.1
 -- http://www.phpmyadmin.net
 --
--- Host: sql212.byetcluster.com
--- Generation Time: Mar 25, 2009 at 01:44 PM
+-- Host: sql212.byethost18.com
+-- Generation Time: Mar 27, 2009 at 12:59 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.6-1+lenny2
 
@@ -30,38 +30,41 @@ CREATE TABLE IF NOT EXISTS `bugs` (
   `Name` varchar(255) NOT NULL,
   `Description` text NOT NULL,
   `DateAdded` datetime NOT NULL,
+  `Module` enum('Skyrates','Skybug') NOT NULL default 'Skyrates',
   `Kind` char(1) NOT NULL default 'B',
-  `Status` enum('Posted','Planned','Working','Finished') NOT NULL,
+  `Status` enum('Posted','Planned','Working','Finished') NOT NULL default 'Posted',
   `Likes` int(11) NOT NULL,
   `Votes` int(11) NOT NULL,
-  `Difference` int(11) NOT NULL,
   `Rate` double NOT NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
 
 --
 -- Dumping data for table `bugs`
 --
 
-INSERT INTO `bugs` (`ID`, `Name`, `Description`, `DateAdded`, `Kind`, `Status`, `Likes`, `Votes`, `Difference`, `Rate`) VALUES
-(2, 'Split Profit misbehaves', 'http://skyrates.net/forum/viewtopic.php?t=6007', '2009-03-25 00:03:38', 'B', 'Posted', 1, 2, 0, 0.5),
-(9, 'Hull upgrades', 'http://skyrates.net/forum/viewtopic.php?t=6423', '2009-03-25 01:02:26', 'F', 'Posted', 8, 10, 6, 0.8),
-(8, 'Fix firepower.', 'http://skyrates.net/forum/viewtopic.php?t=6403', '2009-03-25 00:55:55', 'F', 'Posted', 3, 3, 3, 1),
-(11, 'Avatar Customization', 'Custom outfits, please', '2009-03-25 01:11:06', 'F', 'Posted', 1, 3, -1, 0.333333333),
-(7, 'Extra escape characters', 'Names with apostrophes get an extra \\\\.', '2009-03-25 00:54:36', 'B', 'Posted', 1, 2, 0, 0.5),
-(10, 'Gun Upgrades', 'Gun purchased at Skylands not installed on plane.', '2009-03-25 01:08:55', 'B', 'Posted', 1, 2, 0, 0.5),
-(12, 'Skybug stylesheet', 'Make Skybug pretty', '2009-03-25 01:21:26', 'S', 'Posted', 0, 2, -2, 0),
-(13, 'Skybug filter on kind', 'See only bugs / See only features', '2009-03-25 01:22:06', 'S', 'Posted', 0, 2, -2, 0),
-(14, 'Separate high/low priority votes', 'http://skyrates.net/forum/viewtopic.php?p=65799#65799', '2009-03-25 01:26:21', 'S', 'Posted', 2, 2, 2, 1),
-(15, 'Skybug forum links', 'Skybug automatically link to forum posts, threads', '2009-03-25 01:40:15', 'S', 'Posted', 2, 4, 0, 0.5),
-(16, 'Skybug Kind color-coding', 'Color-code what kind of report it is.', '2009-03-25 01:46:52', 'S', 'Posted', 1, 2, 0, 0.5),
-(17, 'repeat voting on skybug', 'Can vote unlimited number of times in any given period of time.', '2009-03-25 02:40:16', 'S', 'Posted', 1, 3, -1, 0.333333333),
-(18, 'Influence affects Skytopia', 'Make influence have some effect on the game besides the little colored flag.', '2009-03-25 03:33:56', 'F', 'Posted', 3, 5, 1, 0.6),
-(19, 'Wilson confidence', 'http://www.evanmiller.org/how-not-to-sort-by-average-rating.html', '2009-03-25 07:13:44', 'S', 'Posted', 3, 4, 2, 0.75),
-(20, 'Bayesian rating', 'http://www.thebroth.com/blog/118/bayesian-rating', '2009-03-25 07:14:02', 'S', 'Posted', 1, 2, 0, 0.5),
-(22, 'Armor Numbers', 'The little plus sign after the armor should be a number.', '2009-03-25 08:46:39', 'F', 'Posted', 3, 3, 3, 1),
-(23, 'Skyrates is Addictive', 'I should go play outside', '2009-03-25 09:19:41', 'F', 'Posted', 2, 2, 2, 1),
-(24, 'Monster Trucks', 'Vrrroom vrooooommmm!', '2009-03-25 09:39:30', 'F', 'Posted', 2, 2, 2, 1);
+INSERT INTO `bugs` (`ID`, `Name`, `Description`, `DateAdded`, `Module`, `Kind`, `Status`, `Likes`, `Votes`, `Rate`) VALUES
+(2, 'Split Profit misbehaves', '[[Topic:6007]]', '2009-03-25 00:03:38', 'Skyrates', 'B', 'Posted', 3, 7, 0.428571428),
+(9, 'Hull upgrades', '[[Topic:6423]]', '2009-03-25 01:02:26', 'Skyrates', 'F', 'Posted', 27, 33, 0.818181818),
+(8, 'Fix firepower.', '[[Topic:6403]]', '2009-03-25 00:55:55', 'Skyrates', 'F', 'Posted', 9, 15, 0.6),
+(11, 'Avatar Customization', 'Custom outfits, please', '2009-03-25 01:11:06', 'Skyrates', 'F', 'Posted', 11, 17, 0.647058823),
+(7, 'Extra escape characters', 'Names with apostrophes get an extra \\\\.', '2009-03-25 00:54:36', 'Skyrates', 'B', 'Posted', 2, 7, 0.285714285),
+(10, 'Gun Upgrades', 'Gun purchased at Skylands not installed on plane.', '2009-03-25 01:08:55', 'Skyrates', 'B', 'Posted', 13, 21, 0.619047619),
+(12, 'Skybug stylesheet', 'Make Skybug pretty', '2009-03-25 01:21:26', 'Skybug', 'F', 'Posted', 5, 13, 0.384615384),
+(13, 'Skybug filter on kind', 'See only bugs / See only features', '2009-03-25 01:22:06', 'Skybug', 'F', 'Posted', 6, 8, 0.75),
+(49, 'Do Devs see me?', 'Convince me that anything I post here will ever been seen or considered as valuable input to a Dev.', '2009-03-25 17:08:59', 'Skyrates', 'F', 'Posted', 53, 66, 0.803030303),
+(17, 'Skybug authentication', 'Limit votes to one per user per issue', '2009-03-25 02:40:16', 'Skybug', 'F', 'Posted', 13, 20, 0.65),
+(18, 'Integrate influence game', 'Unlikely: see [[Post:65573]]', '2009-03-25 03:33:56', 'Skyrates', 'F', 'Posted', 6, 14, 0.428571428),
+(19, 'Wilson confidence', 'http://www.evanmiller.org/how-not-to-sort-by-average-rating.html', '2009-03-25 07:13:44', 'Skybug', 'F', 'Posted', 11, 14, 0.785714285),
+(20, 'Bayesian rating', 'http://www.thebroth.com/blog/118/bayesian-rating', '2009-03-25 07:14:02', 'Skybug', 'F', 'Posted', 9, 12, 0.75),
+(22, 'Armor and DR', 'Reconfigure armor and DR so that we can see all our armor.', '2009-03-25 08:46:39', 'Skyrates', 'F', 'Posted', 16, 18, 0.888888888),
+(25, 'Bug status column', 'See if anybody''s working on it. (Likely would update slow.)', '2009-03-25 12:08:01', 'Skybug', 'F', 'Posted', 5, 6, 0.833333333),
+(51, 'Rebalance Gun Types', 'Make all the \\&#34;good\\&#34; guns more balanced overall, AC useful', '2009-03-26 00:49:17', 'Skyrates', 'F', 'Posted', 4, 6, 0.666666666),
+(27, 'Skybug Instructions', 'Might keep people from accidentally screwing everything up.', '2009-03-25 12:13:41', 'Skybug', 'F', 'Posted', 6, 8, 0.75),
+(50, 'Inger Combat AI', 'Remove the ADHD subroutine that makes AI ingers start chasing shiny things plz', '2009-03-25 21:55:23', 'Skyrates', 'F', 'Posted', 6, 8, 0.75),
+(52, 'Lose the validation button', 'down the bottom there.', '2009-03-26 08:26:26', 'Skybug', 'F', 'Posted', 2, 3, 0.666666666),
+(53, 'Remove Mission Resetting', 'Make mission reset dependent on a timer, rather than on a round-trip, so fast planes aren\\&#39;t overpowered.', '2009-03-26 14:59:33', 'Skyrates', 'F', 'Posted', 1, 4, 0.25),
+(54, 'Improve Captain Remy\\&#39;s', 'Captain Remy\\&#39;s Craze is losable, and has a much lower Inf/Plane and Money/Plane than the Flawless/Unparalleled/Perfect Record Line', '2009-03-26 17:55:46', 'Skyrates', 'F', 'Posted', 4, 4, 1);
 
 -- --------------------------------------------------------
 
