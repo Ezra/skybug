7653e38b703d134db831798c163389d11adc0494
Ezra Bradford <ezra.bradford@gmail.com>
2009-03-27 04:27:27 -0700

    Some slight HTML tidying.

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 	</head>
 	
 	<body>
-		<h1 style="width: 7em; margin-left: auto; margin-right: auto">
+		<h1 id="heading" style="width: 7em; margin-left: auto; margin-right: auto; text-align: center;">
 			Skybug Tracker
 		</h1>
 		
-		<div style="width:20em; margin-left: auto; margin-right: auto">
+		<div id="submission-form" style="width:20em; margin-left: auto; margin-right: auto">
 			<form action="submit.php" method="post">
 				<fieldset>
 					<div>
@@ -59,92 +59,92 @@
 			</form>
 		</div>
 		
-		<table style="margin-left: auto; margin-right: auto" border="1px">
-			<form action="vote.php" method="post">
-				<tbody>
-					<tr>
-						<th>
-							Priority
-						</th>
-						<th>
-							Name
-						</th>
-						<th>
-							Module
-						</th>
-						<th>
-							Kind
-						</th>
-						<th>
-							Description
-						</th>
-					</tr>
-					<?php
-					require("server.php");
-				
-					if(mysqli_connect_errno()) {
-						echo "Connection Failed: " . mysqli_connect_errno();
-						exit();
-					}
-					
-					if($stmt = $skybug -> prepare("SELECT ID, Name, Description, Module, Kind, Likes, Votes FROM bugs ORDER BY Rate DESC LIMIT 50")) {
-						$stmt -> execute();
-						$stmt -> bind_result($id, $name, $description, $module, $kind, $likes, $votes);
-						while($stmt -> fetch()) {
-							?>
-							<tr>
-								<td style="text-align:center; padding-left:4; padding-right:4">
-									<button onclick="<?="priorityUp(".$id.");" ?>" >+</button>
-									<?= $likes."/".$votes ?>
-									<input type="hidden" name="<?= $id ?>" id="<?= "vote".$id ?>" value="0" />
-									<button onclick="<?="priorityDown(".$id.");" ?>" >-</button>
-								</td>
-								<td style="text-align:center">
-									<?= stripslashes($name) ?>
-								</td>
-								<td style=
-									<?="\"text-align:center; background-color:".
-										(($module=="Skyrates")?"#99CCFF":
-										(($module=="Skybug")?"#FFCC99":
-										$module))."\""?>>
-									<?= $module ?>
-								</td>
-								<td style=
-									<?="\"text-align:center; background-color:".
-										(($kind=="B")?"#FF99CC":
-										(($kind=="F")?"#CCFF99":
-										$kind))."\""?>>
-									<?=
-										(($kind=="B")?"Bug":
-										(($kind=="F")?"Feature":
-										$kind))
-									?>
-								</td>
-								<td style="text-align:center">
-									<?= preg_replace("|\[\[[Pp]ost:(\d+)\]\]|", "<a href=\"http://skyrates.net/forum/viewtopic.php?p=$1#$1\">Post #$1</a>",
-										preg_replace("|\[\[[Tt]opic:(\d+)\]\]|", "<a href=\"http://skyrates.net/forum/viewtopic.php?t=$1\">Topic #$1</a>",
-										stripslashes($description))) ?>
-								</td>
-							</tr>
-							<?php
-						}
-						$stmt -> close();
-					} else {
-						
-						?>
-						<div style="text-align: center">
-							There was an error fetching the bug table. Please try again, or contact Eskay for help.<br />
-							<a href="index.php">return</a>
-						</div>
+		<div style="margin-left:auto; margin-right: auto;">
+			<table border="1px">
+				<form action="vote.php" method="post">
+					<tbody>
+						<tr id="row-head">
+							<th>
+								Priority
+							</th>
+							<th>
+								Name
+							</th>
+							<th>
+								Module
+							</th>
+							<th>
+								Kind
+							</th>
+							<th>
+								Description
+							</th>
+						</tr>
 						<?php
-						
-					}
+						require("server.php");
 					
-					$skybug -> close();
-					?>
-				</tbody>
-			</form>
-		</table>
+						if(mysqli_connect_errno()) {
+							echo "Connection Failed: " . mysqli_connect_errno();
+							exit();
+						}
+						
+						if($stmt = $skybug -> prepare("SELECT ID, Name, Description, Module, Kind, Likes, Votes FROM bugs ORDER BY Rate DESC LIMIT 50")) {
+							$stmt -> execute();
+							$stmt -> bind_result($id, $name, $description, $module, $kind, $likes, $votes);
+							while($stmt -> fetch()) {
+								?>
+								<tr id="row-<?= $id ?>">
+									<td style="text-align:center; padding-left:4; padding-right:4">
+										<button onclick="priorityUp(<?= $id ?>);" >+</button>
+										<?= $likes."/".$votes ?>
+										<input type="hidden" name="<?= $id ?>" id="<?= "vote".$id ?>" value="0" />
+										<button onclick="priorityDown(<?= $id ?>);" >-</button>
+									</td>
+									<td style="text-align:center">
+										<?= stripslashes($name) ?>
+									</td>
+									<td style="text-align:center; background-color:<?=
+											(($module=="Skyrates")?"#99CCFF":
+											(($module=="Skybug")?"#FFCC99":
+											$module)) ?>">
+										<?= $module ?>
+									</td>
+									<td style="text-align:center; background-color:<?=
+											(($kind=="B")?"#FF99CC":
+											(($kind=="F")?"#CCFF99":
+											$kind)) ?>">
+										<?=
+											(($kind=="B")?"Bug":
+											(($kind=="F")?"Feature":
+											$kind))
+										?>
+									</td>
+									<td style="text-align:center">
+										<?= preg_replace("|\[\[[Pp]ost:(\d+)\]\]|", "<a href=\"http://skyrates.net/forum/viewtopic.php?p=$1#$1\">Post #$1</a>",
+											preg_replace("|\[\[[Tt]opic:(\d+)\]\]|", "<a href=\"http://skyrates.net/forum/viewtopic.php?t=$1\">Topic #$1</a>",
+											stripslashes($description))) ?>
+									</td>
+								</tr>
+								<?php
+							}
+							$stmt -> close();
+						} else {
+							
+							?>
+							<div style="text-align: center">
+								There was an error fetching the bug table. Please try again, or contact Eskay for help.<br />
+								<a href="index.php">return</a>
+							</div>
+							<?php
+							
+						}
+						
+						$skybug -> close();
+						?>
+					</tbody>
+				</form>
+			</table>
+		</div>
 		<p style="width:88px; margin-left: auto; margin-right: auto">
 			<a href="http://validator.w3.org/check?uri=referer">
 				<img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" style="border-width:0px;" />
