91e4a8c12e8b805dbf69ee154143dcbfb33d7399
Sohum Banerjea <sohumb@gmail.com>
2010-03-27 19:22:06 +1100

    removing autovote if the user isn't logged in

--- submit.php
+++ submit.php
@@ -4,8 +4,6 @@
 	if(mysqli_connect_errno()) {
 		echo "Connection Failed: " . mysqli_connect_errno();
 		exit();
-	} else {
-		header("Location:index.php");
 	}
 
 	if($stmt = $skybug -> prepare("INSERT INTO bugs (Name, Description, DateAdded, Module, Kind, Likes, Votes, Rate) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
@@ -19,19 +17,31 @@
 		$date = date("Y/m/d H:i:s");
 		$module = $_POST["module"];
 		$kind = $_POST["kind"];
-		$likes = 1;
-		$votes = 1;
-		$rate = 1.0;
+
+		if($loggedin) {
+			$likes = 1;
+			$votes = 1;
+			$rate = 1.0;
+		} else {
+			$likes = 0;
+			$votes = 0;
+			$rate = 0.0;
+		}
 
 		$stmt -> execute();
 		$stmt -> close();
-		if($stmt = $skybug -> prepare("INSERT INTO log (User, Vote, Bug) VALUES (?, ?, ?)")) {
-			$up_string = "up";
-			$stmt -> bind_param('ssi', $username, $up_string, ($skybug -> insert_id));
-			$stmt -> execute();
-			$stmt -> close();
+
+		if (!$loggedin) {
+			$_SESSION['msg'] = "You must be logged in to autovote on issues.";
+		} else {
+			if($stmt = $skybug -> prepare("INSERT INTO log (User, Vote, Bug) VALUES (?, ?, ?)")) {
+				$up_string = "up";
+				$stmt -> bind_param('ssi', $username, $up_string, ($skybug -> insert_id));
+				$stmt -> execute();
+				$stmt -> close();
+			}
 		}
 	}
-
 	$skybug -> close();
+	header("Location:index.php");
 ?>
